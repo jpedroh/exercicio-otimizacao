0848cce41f5e42c7ce36db61ed61f34c194eb407
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2021-08-23 13:28:30 -0300

    implementar mais coisa

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-from math import sqrt
+from math import exp, sqrt
+from random import sample, random
+
+DISTANCIAS = []
 
 def construir_estado_inicial(cidades):
-  pass
+  return list(range(0, len(cidades)))
 
 def calcular_temperatura(t):
-  pass
+  if(t <= 5):
+    return 10
+  elif t <= 9:
+    return 5
+  return 0
 
 def escolher_sucessor(no_atual):
-  pass
+  return sample(no_atual, len(no_atual))
 
 def calcular_valor(no):
-  pass
+  valor = 0
+  for i in range(0, len(no) - 1):
+    valor += DISTANCIAS[i][i+1]
+  return valor
 
 def probabilidade_aleatoria():
-  pass
+  return random()
 
 def calcular_probabilidade_no(delta_e, T):
-  pass
+  return exp(delta_e/T)
 
 def tempera_simulada(cidades):
   no_atual = construir_estado_inicial(cidades)
@@ -38,12 +48,12 @@ def tempera_simulada(cidades):
 
     t += 1
 
+    print(no_atual, calcular_valor(no_atual))
+
 def calcular_distancia(a, b):
   return sqrt((float(a[0]) - float(b[0]))**2 + (float(a[1]) - float(b[1]))**2)
 
 def calcular_distancias(arquivo):
-  matriz_distancias = []
-
   f = open(arquivo)
   linhas = [line.rstrip() for line in f]
   f.close()
@@ -54,11 +64,8 @@ def calcular_distancias(arquivo):
 
     for destino in cidades:
       distancias.append(calcular_distancia([cidade[1], cidade[2]], [destino[1], destino[2]]))
-
-    matriz_distancias.append(distancias)
-
-  return matriz_distancias
+    DISTANCIAS.append(distancias)
 
 if __name__ == "__main__":
-  matriz_distancias = calcular_distancias("dj38.tsp")
-  tempera_simulada([])
+  calcular_distancias("dj38.tsp")
+  tempera_simulada(list(range(0, 38)))
